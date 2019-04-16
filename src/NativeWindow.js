ca02f88e7223b78977a8be792538ccc44846d753
kanata3249 <kanata3249@gmail.com>
2019-04-16 21:03:01 +0900

    Don't open use32/kernel32.dll on MacOS

--- src/NativeWindow.js
+++ src/NativeWindow.js
@@ -1,5 +1,21 @@
 var FFI = require('ffi')
 var ref = require('ref')
+var os = require('os')
+
+require('console')
+
+module.exports = class NativeWindow {
+  constructor() {
+    this.nativeApi = null
+    if (os.type().toString().match('Windows')) {
+      this.nativeApi = new Win32Api()
+    }
+  }
+
+  paste(text) {
+    return this.nativeApi && this.nativeApi.paste(text)
+  }
+}
 
 var int32ptr = ref.refType('int32')
 
@@ -7,48 +23,47 @@ const WM_KEYDOWN = 0x0100
 const WM_KEYUP = 0x0101
 const WM_CHAR = 0x0102
 const WM_IME_CHAR = 0x0286
-var user32 = new FFI.Library('user32', {
-  'GetFocus': [
-    'int32', []
-  ],
-  'GetForegroundWindow': [
-    'int32', []
-  ],
-  'GetWindowThreadProcessId': [
-    'int32', ['int32', int32ptr]
-  ],
-  'AttachThreadInput': [
-    'int32', ['int32', 'int32', 'int32'],
-  ],
-  "PostMessageW": [
-    'int32', ['int32', 'int32', 'int32', 'uint32'],
-  ]
-})
 
-var kernel32 = new FFI.Library('kernel32', {
-  'GetCurrentThreadId': [
-    'int32', []
-  ]
-})
-
-require('console')
-
-module.exports = class NativeWindow {
+class Win32Api {
   constructor() {
+    this.user32 = new FFI.Library('user32', {
+      'GetFocus': [
+        'int32', []
+      ],
+      'GetForegroundWindow': [
+        'int32', []
+      ],
+      'GetWindowThreadProcessId': [
+        'int32', ['int32', int32ptr]
+      ],
+      'AttachThreadInput': [
+        'int32', ['int32', 'int32', 'int32'],
+      ],
+      "PostMessageW": [
+        'int32', ['int32', 'int32', 'int32', 'uint32'],
+      ]
+    })
+    console.log(this.user32)
+
+    this.kernel32 = new FFI.Library('kernel32', {
+      'GetCurrentThreadId': [
+        'int32', []
+      ]
+    })
   }
 
   getFocusWindow() {
     var fgProcessIdPtr = ref.alloc('int32')
     var focusWindow;
 
-    const fgWindow = user32.GetForegroundWindow()
-    const fgThreadId = user32.GetWindowThreadProcessId(fgWindow, fgProcessIdPtr)
-    const myThreadId = kernel32.GetCurrentThreadId()
-    const need_detach = user32.AttachThreadInput(myThreadId, fgThreadId, true)
+    const fgWindow = this.user32.GetForegroundWindow()
+    const fgThreadId = this.user32.GetWindowThreadProcessId(fgWindow, fgProcessIdPtr)
+    const myThreadId = this.kernel32.GetCurrentThreadId()
+    const need_detach = this.user32.AttachThreadInput(myThreadId, fgThreadId, true)
 
-    focusWindow = user32.GetFocus()
+    focusWindow = this.user32.GetFocus()
     if (need_detach) {
-      user32.AttachThreadInput(myThreadId, fgThreadId, false)
+      this.user32.AttachThreadInput(myThreadId, fgThreadId, false)
     }
     if (focusWindow == 0) {
       focusWindow = fgWindow
@@ -65,10 +80,10 @@ module.exports = class NativeWindow {
     }
     const charCode = text.charCodeAt(0)
     if (charCode == 0x0d) {
-      user32.PostMessageW(focusWindow, WM_KEYDOWN, charCode, 0)
-      user32.PostMessageW(focusWindow, WM_KEYUP, charCode, 0xc0000000)
+      this.user32.PostMessageW(focusWindow, WM_KEYDOWN, charCode, 0)
+      this.user32.PostMessageW(focusWindow, WM_KEYUP, charCode, 0xc0000000)
     } else {
-      user32.PostMessageW(focusWindow, WM_IME_CHAR, charCode, 0)
+      this.user32.PostMessageW(focusWindow, WM_IME_CHAR, charCode, 0)
     }
     setTimeout(this.sendTextWithDelay.bind(this), delay, focusWindow, text.slice(1), delay)
   }
